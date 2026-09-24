3b7fed82a4b8cb3bd48e8c82d9fe90fc7ae8986c
Phat Trader <phat@trader.local>
2026-09-24 18:16:16 +0700

    docs: add python check to Dong_Bo_MT5.bat

--- Dong_Bo_MT5.bat
+++ Dong_Bo_MT5.bat
@@ -5,6 +5,16 @@ echo ========================================================
 echo       DANG KET NOI VA DONG BO LENH TU MT5 VAO NHAT KY
 echo ========================================================
 echo.
+
+where python >nul 2>nul
+if %errorlevel% neq 0 (
+    echo [!] May tinh nay chua cai dat Python hoac chua tick chon 'Add Python to PATH'!
+    echo [*] Vui long tai va cai dat Python tai: https://www.python.org/downloads/
+    echo.
+    pause
+    exit /b
+)
+
 python  %~dp0dong_bo_mt5.py
 echo.
 echo ========================================================
